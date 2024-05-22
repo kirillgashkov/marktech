608702bdedc92c5c546e1892209d10925f75633e
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-05-22 23:28:57 +0300

    Add table makers and setters

--- writer.lua
+++ writer.lua
@@ -58,10 +58,10 @@ local function makeMulticol(content, cell, colSpec)
 	}))
 end
 
----@class cell
+---@class cell_
 ---@field Type "cell"
 ---@field Cell Cell
----@class mergeCell
+---@class mergeCell_
 ---@field Type "mergeCell"
 ---@field Of { X: integer, Y: integer }
 
@@ -70,7 +70,7 @@ end
 ---@param colCount integer
 ---@param source string|nil
 local function makeRows(rows, rowCount, colCount, source)
-	---@type List<List<cell | mergeCell | nil>>
+	---@type List<List<cell_ | mergeCell_ | nil>>
 	local t = pandoc.List({})
 	for y = 1, rowCount do
 		local r = pandoc.List({})
@@ -95,10 +95,10 @@ local function makeRows(rows, rowCount, colCount, source)
 
 						local c
 						if y_offset == 0 and x_offset == 0 then
-							---@type cell
+							---@type cell_
 							c = { Type = "cell", Cell = cell }
 						else
-							---@type mergeCell
+							---@type mergeCell_
 							c = { Type = "mergeCell", Of = { X = x, Y = y } }
 						end
 						t[y + y_offset][x + x_offset] = c
@@ -124,7 +124,7 @@ local function makeRows(rows, rowCount, colCount, source)
 			if t[y][x].Type == "cell" then
 				---@type any
 				local c = t[y][x]
-				---@type cell
+				---@type cell_
 				c = c
 
 				local inlinesCell = pandoc.utils.blocks_to_inlines(c.Cell.contents, { pandoc.LineBreak() })
@@ -139,13 +139,13 @@ local function makeRows(rows, rowCount, colCount, source)
 			elseif t[y][x].Type == "mergeCell" then
 				---@type any
 				local c = t[y][x]
-				---@type mergeCell
+				---@type mergeCell_
 				c = c
 
 				---@type any
 				ofC = t[c.Of.Y][c.Of.X]
 				assert(ofC.Type == "cell")
-				---@type cell
+				---@type cell_
 				ofC = ofC
 
 				if y == c.Of.Y then
@@ -164,6 +164,227 @@ local function makeRows(rows, rowCount, colCount, source)
 	end
 
 	return inlinesCellsRows
+end
+
+---@param pa Alignment
+---@return "left" | "center" | "right" | nil
+local function getAlignment(pa)
+	local a
+
+	if pa == "AlignDefault" then
+		a = nil
+	elseif pa == "AlignLeft" then
+		a = "left"
+	elseif pa == "AlignCenter" then
+		a = "center"
+	elseif pa == "AlignRight" then
+		a = "right"
+	else
+		assert(false)
+	end
+
+	return a
+end
+
+---@param colSpecs List<ColSpec>
+---@return List<"left" | "center" | "right">
+local function makeColAlignments(colSpecs)
+	local alignments = pandoc.List({})
+
+	for _, colSpec in ipairs(colSpecs) do
+		local colSpecAlignment = colSpec[1]
+		alignments:insert(getAlignment(colSpecAlignment) or "left")
+	end
+
+	return alignments
+end
+
+-- I'm not sure if Pandoc allows mixing default and non-default column widths but we do. One of our filters surely
+-- generates such tables. Also these widths account for borders, similar to CSS's "box-sizing: border-box".
+---@param colSpecs List<ColSpec>
+---@param source string|nil
+---@return List<"max-width" | number>
+local function makeColWidths(colSpecs, source)
+	local widths = pandoc.List({})
+
+	for _, colSpec in ipairs(colSpecs) do
+		local colSpecWidth = colSpec[2]
+		if colSpecWidth == "ColWidthDefault" then
+			widths:insert("max-width")
+		else
+			assert(type(colSpecWidth) == "number")
+			widths:insert(colSpecWidth)
+		end
+	end
+
+	local totalPercentageWidth = 0
+	for i = 1, #widths do
+		totalPercentageWidth = totalPercentageWidth + widths[i]
+	end
+	if totalPercentageWidth > 1 then
+		log.Warning("the table has a total column width greater than 100%", source)
+	end
+
+	return widths
+end
+
+---@param innerWidth number
+---@param outerWidth number
+---@param firstTop "inner" | "outer" | nil
+---@param lastBottom "inner" | "outer" | nil
+---@param rowCount integer
+---@return List<{ T: number | nil, B: number | nil }>
+local function makeRowBorders(innerWidth, outerWidth, firstTop, lastBottom, rowCount)
+	local borders = pandoc.List({})
+	for i = 1, rowCount do
+		if i == 1 then
+			if firstTop == "inner" then
+				borders:insert({ T = innerWidth })
+			elseif firstTop == "outer" then
+				borders:insert({ T = outerWidth })
+			elseif firstTop == nil then
+				borders:insert({})
+			else
+				assert(false)
+			end
+		elseif i == rowCount then
+			if lastBottom == "inner" then
+				borders:insert({ B = innerWidth })
+			elseif lastBottom == "outer" then
+				borders:insert({ B = outerWidth })
+			elseif lastBottom == nil then
+				borders:insert({})
+			else
+				assert(false)
+			end
+		else
+			borders:insert({ T = innerWidth })
+		end
+	end
+	return borders
+end
+
+---@param innerWidth number
+---@param outerWidth number
+---@param colCount integer
+---@return List<{ L: number | nil, R: number | nil }>
+local function makeColBorders(innerWidth, outerWidth, colCount)
+	local borders = pandoc.List({})
+	for i = 1, colCount do
+		if i == 1 then
+			borders:insert({ L = outerWidth })
+		elseif i == colCount then
+			borders:insert({ R = outerWidth })
+		else
+			borders:insert({ L = innerWidth })
+		end
+	end
+	return borders
+end
+
+---@alias cell contentCell | mergeCell
+---@class contentCell
+---@field Type "contentCell"
+---@field Content Inlines
+---@field Border { T: number | nil, B: number | nil, L: number | nil, R: number | nil }
+---@field Alignment "left" | "center" | "right"
+---@field Width "max-width" | number
+---@field RowSpan integer
+---@field ColSpan integer
+---@class mergeCell
+---@field Type "mergeCell"
+---@field Of { X: integer, Y: integer }
+
+---@param t List<List<cell>>
+---@param rowCount integer
+---@param colCount integer
+---@param rows List<Row> # Used to derive per-cell alignments.
+---@param colAlignments List<"left" | "center" | "right">
+---@return List<List<cell>>
+local function setCellAlignments(t, rowCount, colCount, rows, colAlignments)
+	for y = 1, rowCount do
+		local colIndex = 1
+		for x = 1, colCount do
+			if t[y][x].Type == "contentCell" then
+				t[y][x].Alignment = getAlignment(rows[y].cells[colIndex].alignment) or colAlignments[x]
+				colIndex = colIndex + 1
+			elseif t[y][x].Type == "mergeCell" then
+			else
+				assert(false)
+			end
+		end
+	end
+	return t
+end
+
+---@param t List<List<cell>>
+---@param rowCount integer
+---@param colCount integer
+---@param colWidths List<"max-width" | number>
+---@param source string|nil
+---@return List<List<cell>>
+local function setCellWidths(t, rowCount, colCount, colWidths, source)
+	for y = 1, rowCount do
+		for x = 1, colCount do
+			if t[y][x].Type == "contentCell" then
+				local width = 0
+				local anyMaxWidth = false
+				local anyNumber = false
+
+				for i = 1, t[y][x].ColSpan do
+					local colWidth = colWidths[x + i - 1]
+					if colWidth == "max-width" then
+						anyMaxWidth = true
+					elseif type(colWidth) == "number" then
+						width = width + colWidth
+						anyNumber = true
+					else
+						assert(false)
+					end
+				end
+
+				if anyMaxWidth and anyNumber then
+					log.Error("the table has cell merging with mixed content-based and percentage widths", source)
+					assert(false)
+				elseif anyMaxWidth then
+					t[y][x].Width = "max-width"
+				elseif anyNumber then
+					t[y][x].Width = width
+				else
+					assert(false)
+				end
+			elseif t[y][x].Type == "mergeCell" then
+			else
+				assert(false)
+			end
+		end
+	end
+	return t
+end
+
+---@param t List<List<cell>>
+---@param rowCount integer
+---@param colCount integer
+---@param rowBorders List<{ T: number | nil, B: number | nil }>
+---@param colBorders List<{ L: number | nil, R: number | nil }>
+---@return List<List<cell>>
+local function setCellBorders(t, rowCount, colCount, rowBorders, colBorders)
+	for y = 1, rowCount do
+		for x = 1, colCount do
+			if t[y][x].Type == "contentCell" then
+				t[y][x].Border = {
+					T = rowBorders[y].T,
+					B = rowBorders[y].B,
+					L = colBorders[x].L,
+					R = colBorders[x].R,
+				}
+			elseif t[y][x].Type == "mergeCell" then
+			else
+				assert(false)
+			end
+		end
+	end
+	return t
 end
 
 ---@param t Table
