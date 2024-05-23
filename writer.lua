5732a7dc8d9ef07c9dcc8c6e31e7efa81f317889
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-05-23 12:23:51 +0300

    Add makeTablePartLatex function base

--- writer.lua
+++ writer.lua
@@ -12,65 +12,34 @@ local function getSource(attr)
 	return attr.attributes["data-pos"]
 end
 
----@param cell Cell
----@return boolean
-local function isMultirow(cell)
-	return cell.row_span > 1
-end
+---@class contentCellWithContent
+---@field Type "contentCell"
+---@field Content Inlines
+---@field RowSpan integer
+---@field ColSpan integer
 
----@param content Inlines # Used instead of blocks_to_inlines(cell.contents).
----@param cell Cell
----@param colSpec ColSpec
----@return Inlines
-local function makeMultirow(content, cell, colSpec)
-	return pandoc.Inlines(fun.Flatten({
-		{ pandoc.RawInline("latex", "\\multirow") },
-		{ pandoc.RawInline("latex", "{" .. tostring(cell.row_span) .. "}") },
-		{ pandoc.RawInline("latex", "{" .. "..." .. "}") }, -- FIXME: E.g. "{4em}"
-		fun.Flatten({
-			{ pandoc.RawInline("latex", "{") },
-			content,
-			{ pandoc.RawInline("latex", "}") },
-		}),
-	}))
-end
+---@class contentCellWithContentAlignment: contentCellWithContent
+---@field Alignment "left" | "center" | "right"
 
----@param cell Cell
----@return boolean
-local function isMulticol(cell)
-	return cell.col_span > 1 or cell.alignment ~= "AlignDefault"
-end
+---@class contentCellWithContentAlignmentWidth: contentCellWithContentAlignment
+---@field Width "max-width" | number
 
----@param content Inlines # Used instead of blocks_to_inlines(cell.contents).
----@param cell Cell
----@param colSpec ColSpec
----@return Inlines
-local function makeMulticol(content, cell, colSpec)
-	return pandoc.Inlines(fun.Flatten({
-		{ pandoc.RawInline("latex", "\\multicol") },
-		{ pandoc.RawInline("latex", "{" .. tostring(cell.col_span) .. "}") },
-		{ pandoc.RawInline("latex", "{" .. "..." .. "}") }, -- FIXME: E.g. "{|l|}"
-		fun.Flatten({
-			{ pandoc.RawInline("latex", "{") },
-			content,
-			{ pandoc.RawInline("latex", "}") },
-		}),
-	}))
-end
+---@class contentCellWithContentAlignmentWidthBorder: contentCellWithContentAlignmentWidth
+---@field Border { T: number | nil, B: number | nil, L: number | nil, R: number | nil }
 
----@class cell_
----@field Type "cell"
----@field Cell Cell
----@class mergeCell_
+---@alias contentCell contentCellWithContentAlignmentWidthBorder
+
+---@class mergeCell
 ---@field Type "mergeCell"
 ---@field Of { X: integer, Y: integer }
 
----@param rows List<Row>
 ---@param rowCount integer
 ---@param colCount integer
+---@param rows List<Row>
 ---@param source string|nil
-local function makeRows(rows, rowCount, colCount, source)
-	---@type List<List<cell_ | mergeCell_ | nil>>
+---@return List<List<contentCellWithContent | mergeCell>>
+local function makeTablePartBase(rowCount, colCount, rows, source)
+	---@type List<List<contentCellWithContent | mergeCell | nil>>
 	local t = pandoc.List({})
 	for y = 1, rowCount do
 		local r = pandoc.List({})
@@ -88,17 +57,28 @@ local function makeRows(rows, rowCount, colCount, source)
 
 				for y_offset = 0, cell.row_span do
 					for x_offset = 0, cell.col_span do
-						if t[y + y_offset][x + x_offset] ~= nil then
-							log.Error("the table has overlapping spans", getSource(cell.attr))
+						if y + y_offset > rowCount then
+							log.Error("the table has a cell that spans beyond the row count", source)
 							assert(false)
 						end
+						if x + x_offset > colCount then
+							log.Error("the table has a cell that spans beyond the column count", source)
+							assert(false)
+						end
+						assert(t[y + y_offset][x + x_offset] == nil)
 
 						local c
 						if y_offset == 0 and x_offset == 0 then
-							---@type cell_
-							c = { Type = "cell", Cell = cell }
+							local content = pandoc.utils.blocks_to_inlines(cell.contents, { pandoc.LineBreak() })
+							---@type contentCellWithContent
+							c = {
+								Type = "contentCell",
+								Content = content,
+								RowSpan = cell.row_span,
+								ColSpan = cell.col_span,
+							}
 						else
-							---@type mergeCell_
+							---@type mergeCell
 							c = { Type = "mergeCell", Of = { X = x, Y = y } }
 						end
 						t[y + y_offset][x + x_offset] = c
@@ -110,60 +90,17 @@ local function makeRows(rows, rowCount, colCount, source)
 		end
 	end
 
-	---@type List<List<Inlines>>
-	local inlinesCellsRows = pandoc.List({})
 	for y = 1, rowCount do
-		---@type List<Inlines>
-		local inlinesCells = pandoc.List({})
 		for x = 1, colCount do
-			if t[y][x] == nil then
-				log.Error("the table is incomplete", source)
-				assert(false)
-			end
-
-			if t[y][x].Type == "cell" then
-				---@type any
-				local c = t[y][x]
-				---@type cell_
-				c = c
-
-				local inlinesCell = pandoc.utils.blocks_to_inlines(c.Cell.contents, { pandoc.LineBreak() })
-				if isMultirow(c.Cell) then
-					inlinesCell = makeMultirow(inlinesCell, c.Cell, nil) -- FIXME: colSpec
-				end
-				if isMulticol(c.Cell) then
-					inlinesCell = makeMulticol(inlinesCell, c.Cell, nil) -- FIXME: colSpec
-				end
-
-				inlinesCells:insert(inlinesCell)
-			elseif t[y][x].Type == "mergeCell" then
-				---@type any
-				local c = t[y][x]
-				---@type mergeCell_
-				c = c
-
-				---@type any
-				ofC = t[c.Of.Y][c.Of.X]
-				assert(ofC.Type == "cell")
-				---@type cell_
-				ofC = ofC
-
-				if y == c.Of.Y then
-					local inlinesCell = pandoc.Inlines({})
-					if isMulticol(ofC.Cell) then
-						inlinesCell = makeMulticol(inlinesCell, ofC.Cell, nil) -- FIXME: colSpec
-					end
-
-					inlinesCells:insert(inlinesCell)
-				end
-			else
-				assert(false)
-			end
+			assert(t[y][x] ~= nil)
 		end
-		inlinesCellsRows:insert(inlinesCells)
 	end
+	---@type List<List<any>>
+	t = t
+	---@type List<List<contentCellWithContent | mergeCell>>
+	t = t
 
-	return inlinesCellsRows
+	return t
 end
 
 ---@param pa Alignment
@@ -282,26 +219,14 @@ local function makeColBorders(innerWidth, outerWidth, colCount)
 	return borders
 end
 
----@alias cell contentCell | mergeCell
----@class contentCell
----@field Type "contentCell"
----@field Content Inlines
----@field Border { T: number | nil, B: number | nil, L: number | nil, R: number | nil }
----@field Alignment "left" | "center" | "right"
----@field Width "max-width" | number
----@field RowSpan integer
----@field ColSpan integer
----@class mergeCell
----@field Type "mergeCell"
----@field Of { X: integer, Y: integer }
-
----@param t List<List<cell>>
+---@param t List<List<contentCellWithContent | mergeCell>>
 ---@param rowCount integer
 ---@param colCount integer
 ---@param rows List<Row> # Used to derive per-cell alignments.
 ---@param colAlignments List<"left" | "center" | "right">
----@return List<List<cell>>
+---@return List<List<contentCellWithContentAlignment | mergeCell>>
 local function setCellAlignments(t, rowCount, colCount, rows, colAlignments)
+	---@cast t contentCellWithContentAlignment
 	for y = 1, rowCount do
 		local colIndex = 1
 		for x = 1, colCount do
@@ -317,13 +242,14 @@ local function setCellAlignments(t, rowCount, colCount, rows, colAlignments)
 	return t
 end
 
----@param t List<List<cell>>
+---@param t List<List<contentCellWithContentAlignment | mergeCell>>
 ---@param rowCount integer
 ---@param colCount integer
 ---@param colWidths List<"max-width" | number>
 ---@param source string|nil
----@return List<List<cell>>
+---@return List<List<contentCellWithContentAlignmentWidth | mergeCell>>
 local function setCellWidths(t, rowCount, colCount, colWidths, source)
+	---@cast t contentCellWithContentAlignmentWidth
 	for y = 1, rowCount do
 		for x = 1, colCount do
 			if t[y][x].Type == "contentCell" then
@@ -362,21 +288,22 @@ local function setCellWidths(t, rowCount, colCount, colWidths, source)
 	return t
 end
 
----@param t List<List<cell>>
+---@param t List<List<contentCellWithContentAlignmentWidth | mergeCell>>
 ---@param rowCount integer
 ---@param colCount integer
 ---@param rowBorders List<{ T: number | nil, B: number | nil }>
 ---@param colBorders List<{ L: number | nil, R: number | nil }>
----@return List<List<cell>>
+---@return List<List<contentCellWithContentAlignmentWidthBorder | mergeCell>>
 local function setCellBorders(t, rowCount, colCount, rowBorders, colBorders)
+	---@cast t contentCellWithContentAlignmentWidthBorder
 	for y = 1, rowCount do
 		for x = 1, colCount do
 			if t[y][x].Type == "contentCell" then
 				t[y][x].Border = {
 					T = rowBorders[y].T,
-					B = rowBorders[y].B,
+					B = rowBorders[y + t[y][x].RowSpan - 1].B,
 					L = colBorders[x].L,
-					R = colBorders[x].R,
+					R = colBorders[x + t[y][x].ColSpan - 1].R,
 				}
 			elseif t[y][x].Type == "mergeCell" then
 			else
@@ -385,6 +312,275 @@ local function setCellBorders(t, rowCount, colCount, rowBorders, colBorders)
 		end
 	end
 	return t
+end
+
+---@param rowCount integer
+---@param colCount integer
+---@param rows List<Row>
+---@param colSpecs List<ColSpec>
+---@param source string|nil
+---@return List<List<contentCell|mergeCell>>
+local function makeTablePart(rowCount, colCount, rows, colSpecs, source)
+	local t = makeTablePartBase(rowCount, colCount, rows, source)
+	local colAlignments = makeColAlignments(colSpecs)
+	local colWidths = makeColWidths(colSpecs, source)
+	local innerWidth = 0.5 -- pt
+	local outerWidth = 1 -- pt
+	local firstTop = "inner"
+	local lastBottom = "inner"
+	local rowBorders = makeRowBorders(innerWidth, outerWidth, firstTop, lastBottom, rowCount)
+	local colBorders = makeColBorders(innerWidth, outerWidth, colCount)
+	t = setCellAlignments(t, rowCount, colCount, rows, colAlignments)
+	t = setCellWidths(t, rowCount, colCount, colWidths, source)
+	t = setCellBorders(t, rowCount, colCount, rowBorders, colBorders)
+	return t
+end
+
+---@param borderWidth number
+---@return string
+local function makeBorderWidthLatexString(borderWidth)
+	return string.format("%.4f", borderWidth) .. "pt"
+end
+
+---@param width "max-width" | number
+---@param border { L: number | nil, R: number | nil }
+---@return string | nil
+local function makeWidthLatexString(width, border)
+	if width == "max-width" then
+		return nil
+	elseif type(width) == "number" then
+		return fun.Reduce(
+			function(a, b)
+				return a .. b
+			end,
+			fun.Flatten({
+				fun.Flatten({
+					{ "(" },
+					fun.Flatten({
+						{ "\\real{" },
+						{ string.format("%.4f", width) },
+						{ "}" },
+					}),
+					{ " * " },
+					{ "\\columnwidth" },
+					{ ")" },
+				}),
+				{ " - " },
+				{ makeBorderWidthLatexString((border.L or 0) + (border.R or 0)) },
+			}),
+			""
+		)
+	else
+		assert(false)
+	end
+end
+
+---@param alignment "left" | "center" | "right"
+---@return RawInline
+local function makeMaxWidthAlignmentLatex(alignment)
+	if alignment == "left" then
+		return pandoc.RawInline("latex", "l")
+	elseif alignment == "center" then
+		return pandoc.RawInline("latex", "c")
+	elseif alignment == "right" then
+		return pandoc.RawInline("latex", "r")
+	else
+		assert(false)
+	end
+end
+
+---@param alignment "left" | "center" | "right"
+---@param width "max-width" | number
+---@param border { L: number | nil, R: number | nil }
+---@return RawInline
+local function makeNumberWidthAlignmentLatex(alignment, width, border)
+	local raggedOrCenteringLatex
+	if alignment == "left" then
+		raggedOrCenteringLatex = "\\raggedright"
+	elseif alignment == "center" then
+		raggedOrCenteringLatex = "\\centering"
+	elseif alignment == "right" then
+		raggedOrCenteringLatex = "\\raggedleft"
+	else
+		assert(false)
+	end
+
+	return pandoc.RawInline(
+		"latex",
+		(
+			">{"
+			.. raggedOrCenteringLatex
+			.. "\\arraybackslash"
+			.. "}"
+			.. "p{"
+			.. makeWidthLatexString(width, border)
+			.. "}"
+		)
+	)
+end
+
+---@param alignment "left" | "center" | "right"
+---@param width "max-width" | number
+---@param border { L: number | nil, R: number | nil }
+---@return RawInline
+local function makeAlignmentLatex(alignment, width, border)
+	local latex
+	if width == "max-width" then
+		latex = makeMaxWidthAlignmentLatex(alignment)
+	elseif type(width) == "number" then
+		latex = makeNumberWidthAlignmentLatex(alignment, width, border)
+	else
+		assert(false)
+	end
+	return latex
+end
+
+---@param borderWidth number
+---@return RawInline
+local function makeVerticalBorderLatex(borderWidth)
+	return pandoc.RawInline("latex", "!{\\vrule width " .. makeBorderWidthLatexString(borderWidth) .. "}")
+end
+
+---@param borderWidth number
+---@param startX? integer | nil
+---@param endX? integer | nil
+---@param source string|nil
+---@return RawInline
+local function makeHorizontalBorderLatex(borderWidth, startX, endX, source)
+	if startX ~= nil or endX ~= nil then
+		log.Warning("the table has a partial horizontal border due to spans which is not supported yet", source)
+	end
+	return pandoc.RawInline("latex", "\\specialrule{" .. makeBorderWidthLatexString(borderWidth) .. "}{0pt}{0pt}")
+end
+
+---@param alignment "left" | "center" | "right"
+---@param width "max-width" | number
+---@param border { L: number | nil, R: number | nil }
+---@return Inlines
+local function makeColSpecLatex(alignment, width, border)
+	return pandoc.Inlines(fun.Flatten({
+		border.L and { makeVerticalBorderLatex(border.L) } or {},
+		{ makeAlignmentLatex(alignment, width, border) },
+		border.R and { makeVerticalBorderLatex(border.R) } or {},
+	}))
+end
+
+---@param c contentCell
+---@return boolean
+local function isMultirow(c)
+	return c.RowSpan > 1
+end
+
+---@param content Inlines # Used instead of c.content.
+---@param c contentCell
+---@return Inlines
+local function makeMultirowLatex(content, c)
+	return pandoc.Inlines(fun.Flatten({
+		{ pandoc.RawInline("latex", "\\multirow") },
+		{ pandoc.RawInline("latex", "{" .. tostring(c.RowSpan) .. "}") },
+		fun.Flatten({
+			{ pandoc.RawInline("latex", "{") },
+			{ pandoc.RawInline("latex", makeWidthLatexString(c.Width, c.Border) or "*") }, -- "*" means natural width.
+			{ pandoc.RawInline("latex", "}") },
+		}),
+		fun.Flatten({
+			{ pandoc.RawInline("latex", "{") },
+			content,
+			{ pandoc.RawInline("latex", "}") },
+		}),
+	}))
+end
+
+---@param c contentCell
+---@param x integer # The column index.
+---@param colAlignments List<"left" | "center" | "right">
+---@return boolean
+local function isMulticol(c, x, colAlignments)
+	return c.ColSpan > 1 or c.Alignment ~= colAlignments[x]
+end
+
+---@param content Inlines # Used instead of c.content.
+---@param c contentCell
+---@return Inlines
+local function makeMulticolLatex(content, c)
+	return pandoc.Inlines(fun.Flatten({
+		{ pandoc.RawInline("latex", "\\multicol") },
+		{ pandoc.RawInline("latex", "{" .. tostring(c.ColSpan) .. "}") },
+		fun.Flatten({
+			{ pandoc.RawInline("latex", "{") },
+			makeColSpecLatex(c.Alignment, c.Width, c.Border),
+			{ pandoc.RawInline("latex", "}") },
+		}),
+		fun.Flatten({
+			{ pandoc.RawInline("latex", "{") },
+			content,
+			{ pandoc.RawInline("latex", "}") },
+		}),
+	}))
+end
+
+---@param t List<List<contentCell | mergeCell>>
+---@param rowCount integer
+---@param colCount integer
+---@param colAlignments List<"left" | "center" | "right">
+local function makeTablePartLatex(t, rowCount, colCount, colAlignments, rowBorders)
+	---@type List<List<Inlines>>
+	local rows = pandoc.List({})
+
+	for y = 1, rowCount do
+		---@type List<Inlines>
+		local row = pandoc.List({})
+
+		for x = 1, colCount do
+			assert(t[y][x] ~= nil)
+
+			local c = t[y][x]
+			if c.Type == "contentCell" then
+				---@cast c contentCell
+
+				local cell = c.Content
+				if isMultirow(c) then
+					cell = makeMultirowLatex(cell, c)
+				end
+				if isMulticol(c, x, colAlignments) then
+					cell = makeMulticolLatex(cell, c)
+				end
+				row:insert(cell)
+			elseif t[y][x].Type == "mergeCell" then
+				---@cast c mergeCell
+
+				local ofC = t[c.Of.Y][c.Of.X]
+				assert(ofC.Type == "contentCell")
+				---@cast ofC contentCell
+
+				if y == c.Of.Y then
+					local cell = pandoc.Inlines({})
+					if isMulticol(ofC, c.Of.X, colAlignments) then
+						inlinesCell = makeMulticolLatex(inlinesCell, ofC)
+					end
+					row:insert(cell)
+				end
+			else
+				assert(false)
+			end
+		end
+		rows:insert(row)
+	end
+
+	local inlinesList = pandoc.List({})
+
+	for i = 1, rowCount do
+		-- rowBorders[i].T
+		local rowInlines = fun.Reduce(function(reduced, a)
+			if #reduced > 0 then
+				reduced:insert(pandoc.RawInline("latex", "&"))
+			end
+			reduced:embed(a)
+			return reduced
+		end, rows[i], pandoc.Inlines({}))
+	end
+
+	return rows
 end
 
 ---@param t Table
