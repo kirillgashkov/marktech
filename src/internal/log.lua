8c85608b4c48ad9f9c4a11f926959156a403d33a
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-05-27 23:41:30 +0300

    Allow message to be anything in log

--- src/internal/log.lua
+++ src/internal/log.lua
@@ -45,17 +45,37 @@ local function makeJsonLog(type, message, source, code)
   return pandoc.json.encode(d)
 end
 
+local function dump(o)
+  if type(o) == "table" then
+    local s = "{ "
+    local i = 1
+    for k, v in pairs(o) do
+      if i ~= 1 then
+        s = s .. ", "
+      end
+      s = s .. "[" .. dump(k) .. "] = " .. dump(v)
+      i = i + 1
+    end
+    return s .. " }"
+  elseif type(o) == "string" then
+    return '"' .. o .. '"'
+  else
+    return tostring(o)
+  end
+end
+
 ---@param type "panic" | "error" | "warning" | "note"
----@param message string
+---@param message any
 ---@param source? string
 ---@param code? string|nil
 function log.message(type, message, source, code)
   local s = ""
 
+  local m = dump(message)
   if log.Format == "text" then
-    s = makeTextLog(type, message, source, code)
+    s = makeTextLog(type, m, source, code)
   elseif log.Format == "json" then
-    s = makeJsonLog(type, message, source, code)
+    s = makeJsonLog(type, m, source, code)
   else
     assert(false)
   end
@@ -63,21 +83,21 @@ function log.message(type, message, source, code)
   io.stderr:write(s .. "\n")
 end
 
----@param message string
+---@param message any
 ---@param source? string|nil
 ---@param code? string|nil
 function log.Error(message, source, code)
   log.message("error", message, source, code)
 end
 
----@param message string
+---@param message any
 ---@param source? string|nil
 ---@param code? string|nil
 function log.Warning(message, source, code)
   log.message("warning", message, source, code)
 end
 
----@param message string
+---@param message any
 ---@param source? string|nil
 ---@param code? string|nil
 function log.Note(message, source, code)
