aaf6b2670a303d445a2070bcdbacdc8a7e4cc715
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-05-27 21:01:36 +0300

    Add tbl, tableConfig types

--- types/internal.lua
+++ types/internal.lua
@@ -18,10 +18,36 @@
 ---@field Border { T: length, B: length, L: length, R: length }
 
 ---@alias contentCell contentCellWithContentAlignmentWidthBorder
+---@alias cell contentCell | mergeCell
 
 ---@class mergeCell
 ---@field Type "mergeCell"
 ---@field Of { X: integer, Y: integer }
 
 ---@class config
----@field arrayRuleWidth length # https://tex.stackexchange.com/questions/122956/how-thick-a-rule-does-hline-produce
+---@field ArrayRuleWidth length # https://tex.stackexchange.com/questions/122956/how-thick-a-rule-does-hline-produce
+
+---@class tbl
+---@field Id string
+---@field Caption Inline | nil
+---@field ColAlignments List<"left" | "center" | "right">
+---@field ColWidths List<length | nil>
+---@field ColBorders List<{ L: length, R: length }>
+---@field FirstTopBorder length
+---@field LastBottomBorder length
+---@field HeadRows List<List<cell>>
+---@field BodyRows List<List<cell>>
+---@field FootRows List<List<cell>>
+
+---@class tableConfig
+---@field OuterBorderWidth length
+---@field InnerBorderWidth length
+---@field SeparateHead boolean
+---@field RepeatHead boolean
+---@field SeparateFoot boolean
+---@field RepeatFoot boolean
+
+---@class colSpec
+---@field Alignment "left" | "center" | "right"
+---@field Width length | nil
+---@field Border { L: length, R: length }
