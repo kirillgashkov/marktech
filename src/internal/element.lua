2d101b9b626fe445368b830d14c436e7816cee43
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-05-27 21:00:32 +0300

    Add GetSource, Raw, Md, MdBlock functions to element

--- src/internal/element.lua
+++ src/internal/element.lua
@@ -1,5 +1,13 @@
 local element = {}
 
+local mdFormat = "gfm-yaml_metadata_block"
+
+---@param e { attr: Attr }
+---@return string|nil
+function element.GetSource(e)
+	return e.attr.attributes["data-pos"]
+end
+
 ---@param attr Attr
 ---@return boolean
 function element.IsMerge(attr)
@@ -20,6 +28,24 @@ function element.MergeBlock(blocks)
 	local b = pandoc.Div(blocks)
 	b.attr.attributes["data-template--is-merge"] = "1"
 	return b
+end
+
+---@param s string
+---@return Inline
+function element.Raw(s)
+	return pandoc.RawInline("latex", s)
+end
+
+---@param s string
+---@return Inline
+function element.Md(s)
+	return element.Merge(pandoc.utils.blocks_to_inlines(pandoc.read(s, mdFormat).blocks))
+end
+
+---@param s string
+---@return Block
+function element.MdBlock(s)
+	return element.MergeBlock(pandoc.read(s, mdFormat).blocks)
 end
 
 element.MergeFilter = {
